6bd3989a4aa0abeb30f0224c375926c521ef237a
root <root@localhost.localdomain>
2022-02-23 22:46:31 -0500

    small changes on clear_l3.c

--- control_exp/clear_l3.c
+++ control_exp/clear_l3.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<memory.h>
-#define SIZE 144000000
+#define SIZE 72000000
 int main() {
     char *a = malloc(SIZE);
     char *b = malloc(SIZE);
     int i = 0;
     for(i = 0 ; i < SIZE ; i ++) {
-        a[i] = i;
+        a[i] = rand() % 128;
     }
-    for(i = 0 ; i < 3 ; i ++) {
+    for(i = 0 ; i < 1 ; i ++) {
         memcpy(b, a, SIZE);
     }
     return 0;
